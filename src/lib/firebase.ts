57757191e2e9e004a4d46a275060d56e6839bbed
Nik Popov <apis.popov@gmail.com>
2025-09-03 21:55:22 +0000

    wtf......................

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: 'AIzaSyABIMubCSQUjxmJU3pOY_dy_Ui8JAlVh5E',
   authDomain: 'kvm-cloud-rentals.firebaseapp.com',
   messagingSenderId: '186409941941',
-  measurementId: '',
+  measurementId: 'G-XXXXXXXXXX',
 };
 
 // Initialize Firebase
