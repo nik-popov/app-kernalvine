5fb82292ea6c218fe1e96cdc0b62f313094f19ec
Nik Popov <apis.popov@gmail.com>
2025-09-03 21:59:11 +0000

    {   "stack": "FirebaseError: Firebase: Error (auth/configuration-not-fou

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'kvm-cloud-rentals',
-  appId: '1:186409941941:web:026fe4d5f285a2b6a3f7fc',
-  storageBucket: 'kvm-cloud-rentals.firebasestorage.app',
-  apiKey: 'AIzaSyABIMubCSQUjxmJU3pOY_dy_Ui8JAlVh5E',
-  authDomain: 'kvm-cloud-rentals.firebaseapp.com',
-  messagingSenderId: '186409941941',
+  apiKey: "AIzaSyABIMubCSQUjxmJU3pOY_dy_Ui8JAlVh5E",
+  authDomain: "kvm-cloud-rentals.firebaseapp.com",
+  projectId: "kvm-cloud-rentals",
+  storageBucket: "kvm-cloud-rentals.firebasestorage.app",
+  messagingSenderId: "186409941941",
+  appId: "1:186409941941:web:026fe4d5f285a2b6a3f7fc",
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
